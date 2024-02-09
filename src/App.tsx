2b95945fa73a6034ecaa81b680a592b5ba1c0432
Alfredo Monteiro <amonteiro.alfredo@gmail.com>
2024-02-09 05:58:50 -0100

    add header styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,63 @@
 export default function App() {
-  return <p>Hello World</p>;
+  return (
+    <header className="header">
+      <div className="header__top">
+        <a href="/" className="logo">
+          <svg
+            className="logo__icon"
+            width="15"
+            height="15"
+            viewBox="0 0 15 15"
+            fill="none"
+          >
+            <path
+              d="M1.20308 1.04312C1.00481 0.954998 0.772341 1.0048 0.627577 1.16641C0.482813 1.32802 0.458794 1.56455 0.568117 1.75196L3.92115 7.50002L0.568117 13.2481C0.458794 13.4355 0.482813 13.672 0.627577 13.8336C0.772341 13.9952 1.00481 14.045 1.20308 13.9569L14.7031 7.95693C14.8836 7.87668 15 7.69762 15 7.50002C15 7.30243 14.8836 7.12337 14.7031 7.04312L1.20308 1.04312ZM4.84553 7.10002L2.21234 2.586L13.2689 7.50002L2.21234 12.414L4.84552 7.90002H9C9.22092 7.90002 9.4 7.72094 9.4 7.50002C9.4 7.27911 9.22092 7.10002 9 7.10002H4.84553Z"
+              fill="currentColor"
+              fillRule="evenodd"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+          jobs<span className="fw-700 fs-italic">finder</span>
+        </a>
+        <span className="header__separator">|</span>
+        <button className="bookmark">
+          bookmarks
+          <svg
+            className="bookmark__icon"
+            width="15"
+            height="15"
+            viewBox="0 0 15 15"
+            fill="none"
+          >
+            <path d="M4 6H11L7.5 10.5L4 6Z" fill="currentColor"></path>
+          </svg>
+        </button>
+      </div>
+      <form className="search-form" action="#">
+        <div className="search-form__content">
+          <input
+            type="text"
+            className="search-form__input"
+            placeholder="Find remote developer jobs..."
+          />
+          <button type="submit" className="search-form__button">
+            <svg
+              className="search-form__icon"
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              fill="none"
+            >
+              <path
+                d="M10 6.5C10 8.433 8.433 10 6.5 10C4.567 10 3 8.433 3 6.5C3 4.567 4.567 3 6.5 3C8.433 3 10 4.567 10 6.5ZM9.30884 10.0159C8.53901 10.6318 7.56251 11 6.5 11C4.01472 11 2 8.98528 2 6.5C2 4.01472 4.01472 2 6.5 2C8.98528 2 11 4.01472 11 6.5C11 7.56251 10.6318 8.53901 10.0159 9.30884L12.8536 12.1464C13.0488 12.3417 13.0488 12.6583 12.8536 12.8536C12.6583 13.0488 12.3417 13.0488 12.1464 12.8536L9.30884 10.0159Z"
+                fill="currentColor"
+                fillRule="evenodd"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </form>
+    </header>
+  );
 }
