4e61b536a183df91a99a4b93836f81dc1ca87eb0
Alfredo Monteiro <amonteiro.alfredo@gmail.com>
2024-02-11 10:58:24 -0100

    style job list

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,226 @@
 export default function App() {
   return (
-    <header className="header">
-      <div className="header__top">
-        <a href="/" className="logo">
-          <svg
-            className="logo__icon"
-            width="15"
-            height="15"
-            viewBox="0 0 15 15"
-            fill="none"
-          >
-            <path
-              d="M1.20308 1.04312C1.00481 0.954998 0.772341 1.0048 0.627577 1.16641C0.482813 1.32802 0.458794 1.56455 0.568117 1.75196L3.92115 7.50002L0.568117 13.2481C0.458794 13.4355 0.482813 13.672 0.627577 13.8336C0.772341 13.9952 1.00481 14.045 1.20308 13.9569L14.7031 7.95693C14.8836 7.87668 15 7.69762 15 7.50002C15 7.30243 14.8836 7.12337 14.7031 7.04312L1.20308 1.04312ZM4.84553 7.10002L2.21234 2.586L13.2689 7.50002L2.21234 12.414L4.84552 7.90002H9C9.22092 7.90002 9.4 7.72094 9.4 7.50002C9.4 7.27911 9.22092 7.10002 9 7.10002H4.84553Z"
-              fill="currentColor"
-              fillRule="evenodd"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-          jobs<span className="fw-700 fs-italic">finder</span>
-        </a>
-        <span className="header__separator">|</span>
-        <button className="bookmark">
-          bookmarks
-          <svg
-            className="bookmark__icon"
-            width="15"
-            height="15"
-            viewBox="0 0 15 15"
-            fill="none"
-          >
-            <path d="M4 6H11L7.5 10.5L4 6Z" fill="currentColor"></path>
-          </svg>
-        </button>
-      </div>
-      <form className="search-form" action="#">
-        <div className="search-form__content">
-          <input
-            type="text"
-            className="search-form__input"
-            placeholder="Find remote developer jobs..."
-          />
-          <button type="submit" className="search-form__button">
+    <>
+      <header className="header">
+        <div className="header__top">
+          <a href="/" className="logo">
             <svg
-              className="search-form__icon"
+              className="logo__icon"
               width="15"
               height="15"
               viewBox="0 0 15 15"
               fill="none"
             >
               <path
-                d="M10 6.5C10 8.433 8.433 10 6.5 10C4.567 10 3 8.433 3 6.5C3 4.567 4.567 3 6.5 3C8.433 3 10 4.567 10 6.5ZM9.30884 10.0159C8.53901 10.6318 7.56251 11 6.5 11C4.01472 11 2 8.98528 2 6.5C2 4.01472 4.01472 2 6.5 2C8.98528 2 11 4.01472 11 6.5C11 7.56251 10.6318 8.53901 10.0159 9.30884L12.8536 12.1464C13.0488 12.3417 13.0488 12.6583 12.8536 12.8536C12.6583 13.0488 12.3417 13.0488 12.1464 12.8536L9.30884 10.0159Z"
+                d="M1.20308 1.04312C1.00481 0.954998 0.772341 1.0048 0.627577 1.16641C0.482813 1.32802 0.458794 1.56455 0.568117 1.75196L3.92115 7.50002L0.568117 13.2481C0.458794 13.4355 0.482813 13.672 0.627577 13.8336C0.772341 13.9952 1.00481 14.045 1.20308 13.9569L14.7031 7.95693C14.8836 7.87668 15 7.69762 15 7.50002C15 7.30243 14.8836 7.12337 14.7031 7.04312L1.20308 1.04312ZM4.84553 7.10002L2.21234 2.586L13.2689 7.50002L2.21234 12.414L4.84552 7.90002H9C9.22092 7.90002 9.4 7.72094 9.4 7.50002C9.4 7.27911 9.22092 7.10002 9 7.10002H4.84553Z"
                 fill="currentColor"
                 fillRule="evenodd"
                 clipRule="evenodd"
               ></path>
             </svg>
+            jobs<span className="fw-700 fs-italic">finder</span>
+          </a>
+          <span className="header__separator">|</span>
+          <button className="bookmark">
+            bookmarks
+            <svg
+              className="bookmark__icon"
+              width="15"
+              height="15"
+              viewBox="0 0 15 15"
+              fill="none"
+            >
+              <path d="M4 6H11L7.5 10.5L4 6Z" fill="currentColor"></path>
+            </svg>
           </button>
         </div>
-      </form>
-    </header>
+        <form className="search-form" action="#">
+          <div className="search-form__content">
+            <input
+              type="text"
+              className="search-form__input"
+              placeholder="Find remote developer jobs..."
+            />
+            <button type="submit" className="search-form__button">
+              <svg
+                className="search-form__icon"
+                width="15"
+                height="15"
+                viewBox="0 0 15 15"
+                fill="none"
+              >
+                <path
+                  d="M10 6.5C10 8.433 8.433 10 6.5 10C4.567 10 3 8.433 3 6.5C3 4.567 4.567 3 6.5 3C8.433 3 10 4.567 10 6.5ZM9.30884 10.0159C8.53901 10.6318 7.56251 11 6.5 11C4.01472 11 2 8.98528 2 6.5C2 4.01472 4.01472 2 6.5 2C8.98528 2 11 4.01472 11 6.5C11 7.56251 10.6318 8.53901 10.0159 9.30884L12.8536 12.1464C13.0488 12.3417 13.0488 12.6583 12.8536 12.8536C12.6583 13.0488 12.3417 13.0488 12.1464 12.8536L9.30884 10.0159Z"
+                  fill="currentColor"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </form>
+      </header>
+      <main>
+        <section className="jobs">
+          <div className="jobs__header">
+            <h1 className="jobs__title">
+              Bookmarks <span className="fw-400 fs-23px ">(5)</span>
+            </h1>
+          </div>
+        </section>
+        <section className="jobs">
+          <div className="jobs__header">
+            <h1 className="jobs__title">
+              Jobs <span className="fw-400 fs-23px ">(41)</span>
+            </h1>
+            <section className="jobs-sort">
+              <svg
+                className="jobs-sort__icon"
+                width="15"
+                height="15"
+                viewBox="0 0 15 15"
+                fill="none"
+              >
+                <path
+                  d="M4.93179 5.43179C4.75605 5.60753 4.75605 5.89245 4.93179 6.06819C5.10753 6.24392 5.39245 6.24392 5.56819 6.06819L7.49999 4.13638L9.43179 6.06819C9.60753 6.24392 9.89245 6.24392 10.0682 6.06819C10.2439 5.89245 10.2439 5.60753 10.0682 5.43179L7.81819 3.18179C7.73379 3.0974 7.61933 3.04999 7.49999 3.04999C7.38064 3.04999 7.26618 3.0974 7.18179 3.18179L4.93179 5.43179ZM10.0682 9.56819C10.2439 9.39245 10.2439 9.10753 10.0682 8.93179C9.89245 8.75606 9.60753 8.75606 9.43179 8.93179L7.49999 10.8636L5.56819 8.93179C5.39245 8.75606 5.10753 8.75606 4.93179 8.93179C4.75605 9.10753 4.75605 9.39245 4.93179 9.56819L7.18179 11.8182C7.35753 11.9939 7.64245 11.9939 7.81819 11.8182L10.0682 9.56819Z"
+                  fill="currentColor"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              <button className="jobs-sort__button jobs-sort__button--relevant">
+                Relevant
+              </button>
+              <button className="jobs-sort__button jobs-sort__button--recent">
+                Recent
+              </button>
+            </section>
+          </div>
+          <section className="jobs__body">
+            <button className="jobs__button jobs__button--previous">
+              <svg
+                className="jobs__icon"
+                width="15"
+                height="15"
+                viewBox="0 0 15 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M8.84182 3.13514C9.04327 3.32401 9.05348 3.64042 8.86462 3.84188L5.43521 7.49991L8.86462 11.1579C9.05348 11.3594 9.04327 11.6758 8.84182 11.8647C8.64036 12.0535 8.32394 12.0433 8.13508 11.8419L4.38508 7.84188C4.20477 7.64955 4.20477 7.35027 4.38508 7.15794L8.13508 3.15794C8.32394 2.95648 8.64036 2.94628 8.84182 3.13514Z"
+                  fill="currentColor"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </button>
+            <ul className="jobs__list">
+              <li className="jobs__item job-item">
+                <div className="job-item__badge-letter">AS</div>
+                <section className="job-item__info">
+                  <p className="job-item__title">Front-end Developer - React</p>
+                  <p className="job-item__company">AT Security</p>
+                </section>
+                <div className="job-item__col">
+                  <button className="job-item__bookmark">
+                    <svg
+                      className="job-item__icon"
+                      width="15"
+                      height="15"
+                      viewBox="0 0 15 15"
+                      fill="none"
+                    >
+                      <path
+                        d="M3.5 2C3.22386 2 3 2.22386 3 2.5V13.5C3 13.6818 3.09864 13.8492 3.25762 13.9373C3.41659 14.0254 3.61087 14.0203 3.765 13.924L7.5 11.5896L11.235 13.924C11.3891 14.0203 11.5834 14.0254 11.7424 13.9373C11.9014 13.8492 12 13.6818 12 13.5V2.5C12 2.22386 11.7761 2 11.5 2H3.5Z"
+                        fill="currentColor"
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                  </button>
+                  <p className="job-item__age">4d</p>
+                </div>
+              </li>
+              <li className="jobs__item job-item job-item--active">
+                <div className="job-item__badge-letter">9T</div>
+                <section className="job-item__info">
+                  <p className="job-item__title">Front-end React Engineer</p>
+                  <p className="job-item__company">9th Tech</p>
+                </section>
+                <div className="job-item__col">
+                  <button className="job-item__bookmark">
+                    <svg
+                      className="job-item__icon"
+                      width="15"
+                      height="15"
+                      viewBox="0 0 15 15"
+                      fill="none"
+                    >
+                      <path
+                        d="M3.5 2C3.22386 2 3 2.22386 3 2.5V13.5C3 13.6818 3.09864 13.8492 3.25762 13.9373C3.41659 14.0254 3.61087 14.0203 3.765 13.924L7.5 11.5896L11.235 13.924C11.3891 14.0203 11.5834 14.0254 11.7424 13.9373C11.9014 13.8492 12 13.6818 12 13.5V2.5C12 2.22386 11.7761 2 11.5 2H3.5Z"
+                        fill="currentColor"
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                  </button>
+                  <p className="job-item__age">2d</p>
+                </div>
+              </li>
+              <li className="jobs__item job-item">
+                <div className="job-item__badge-letter">AT</div>
+                <section className="job-item__info">
+                  <p className="job-item__title">Junior Software devolper</p>
+                  <p className="job-item__company">Aspen Tech</p>
+                </section>
+                <div className="job-item__col">
+                  <button className="job-item__bookmark">
+                    <svg
+                      className="job-item__icon"
+                      width="15"
+                      height="15"
+                      viewBox="0 0 15 15"
+                      fill="none"
+                    >
+                      <path
+                        d="M3.5 2C3.22386 2 3 2.22386 3 2.5V13.5C3 13.6818 3.09864 13.8492 3.25762 13.9373C3.41659 14.0254 3.61087 14.0203 3.765 13.924L7.5 11.5896L11.235 13.924C11.3891 14.0203 11.5834 14.0254 11.7424 13.9373C11.9014 13.8492 12 13.6818 12 13.5V2.5C12 2.22386 11.7761 2 11.5 2H3.5Z"
+                        fill="currentColor"
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                  </button>
+                  <p className="job-item__age">4d</p>
+                </div>
+              </li>
+            </ul>
+            <button className="jobs__button jobs__next">
+              <svg
+                className="jobs__icon"
+                width="15"
+                height="15"
+                viewBox="0 0 15 15"
+                fill="none"
+              >
+                <path
+                  d="M6.1584 3.13508C6.35985 2.94621 6.67627 2.95642 6.86514 3.15788L10.6151 7.15788C10.7954 7.3502 10.7954 7.64949 10.6151 7.84182L6.86514 11.8418C6.67627 12.0433 6.35985 12.0535 6.1584 11.8646C5.95694 11.6757 5.94673 11.3593 6.1356 11.1579L9.565 7.49985L6.1356 3.84182C5.94673 3.64036 5.95694 3.32394 6.1584 3.13508Z"
+                  fill="currentColor"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </button>
+          </section>
+        </section>
+        <section className="job-details">
+          <img
+            className="job-details__img"
+            src="company-office.jpg"
+            alt="background image"
+          />
+          <h1>Junior Software Developer</h1>
+        </section>
+      </main>
+    </>
   );
 }
